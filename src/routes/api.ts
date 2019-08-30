29f5b73290ea0f797abe943143e5a16161ebbeab
Bruno Barroso <brunobinfo@gmail.com>
2019-08-30 07:41:43 +0100

    Implementing the routes with middlewares

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -2,9 +2,22 @@ import * as express from "express";
 
 export const register = ( app: express.Application ) => {
 
+    const isAuthenticated = {
+        sub: "00abc12defg3hij4k5l6",
+        // tslint:disable-next-line:object-literal-sort-keys
+        name: "First Last",
+        locale: "eu-EU",
+        preferred_username: "account@company.com",
+        given_name: "First",
+        family_name: "Last",
+        zoneinfo: "Europe/Lisbon",
+        updated_at: 1539283620
+    };
+
     // define a route handler for the default home page
     app.get( "/", ( req: any, res ) => {
-        res.render( "index" );
+        const user = req.userContext ? req.userContext.userinfo : null;
+        res.render( "index", { isAuthenticated, user } );
     } );
 
     // define a secure route handler for the login page that redirects to /guitars
@@ -20,6 +33,8 @@ export const register = ( app: express.Application ) => {
 
     // define a secure route handler for the guitars page
     app.get( "/guitars", ( req: any, res ) => {
-        res.render( "guitars" );
+        const user = req.userContext ? req.userContext.userinfo : null;
+        // tslint:disable-next-line: object-literal-shorthand
+        res.render( "guitars", { isAuthenticated: isAuthenticated, user } );
     } );
 };
